bf8a3dc0c5be104088f77857c18627b9126522b9
angel <dehainaut.angelique@orange.fr>
2022-02-24 11:21:57 +0100

    end

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Exo-196-PHP-SQL-les-fonctions-d-agregation
+
+/**
+* 1. Importez le contenu du fichier user.sql dans une nouvelle base de données.
+* 2. Utilisez un des objets de connexion que nous avons fait ensemble pour vous connecter à votre base de données.
+*
+* Pour chaque résultat de requête, affichez les informations, ex:  Age minimum: 36 ans <br>   ( pour obtenir une information par ligne ).
+*
+* 3. Récupérez l'age minimum des utilisateurs.
+* 4. Récupérez l'âge maximum des utilisateurs.
+* 5. Récupérez le nombre total d'utilisateurs dans la table à l'aide de la fonction d'agrégation COUNT().
+* 6. Récupérer le nombre d'utilisateurs ayant un numéro de rue plus grand ou égal à 5.
+    * 7. Récupérez la moyenne d'âge des utilisateurs.
+    * 8. Récupérer la somme des numéros de maison des utilisateurs ( bien que ca n'ait pas de sens ).
+         */
--- index.php
+++ index.php
@@ -1,29 +1,72 @@
 <!doctype html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Fonction d'agrégation</title>
 </head>
 <body>
     <?php
-    /**
-     * 1. Importez le contenu du fichier user.sql dans une nouvelle base de données.
-     * 2. Utilisez un des objets de connexion que nous avons fait ensemble pour vous connecter à votre base de données.
-     *
-     * Pour chaque résultat de requête, affichez les informations, ex:  Age minimum: 36 ans <br>   ( pour obtenir une information par ligne ).
-     *
-     * 3. Récupérez l'age minimum des utilisateurs.
-     * 4. Récupérez l'âge maximum des utilisateurs.
-     * 5. Récupérez le nombre total d'utilisateurs dans la table à l'aide de la fonction d'agrégation COUNT().
-     * 6. Récupérer le nombre d'utilisateurs ayant un numéro de rue plus grand ou égal à 5.
-     * 7. Récupérez la moyenne d'âge des utilisateurs.
-     * 8. Récupérer la somme des numéros de maison des utilisateurs ( bien que ca n'ait pas de sens ).
-     */
+    require 'Connect.php';
+    require 'Config.php';
 
-    // TODO Votre code ici, commencez par require un des objet de connexion que nous avons fait ensemble.
+    $myConnexion = Connect::dbConnect();
+
+    try {
+        $stmt = $myConnexion->prepare("SELECT MIN(age) as minimum FROM user");
+        $state = $stmt->execute();
+
+        if ($state) {
+            $min = $stmt->fetch();
+            echo "Le plus petit âge trouvé est: " . $min['minimum']. "ans" . "<br>";
+        }
+
+        $stmt = $myConnexion->prepare("SELECT MAX(age) as maximum FROM user");
+        $state = $stmt->execute();
+
+        if ($state) {
+            $max = $stmt->fetch();
+            echo "Le plus grand âge trouvé est: " . $max['maximum']. "ans". "<br>";
+        }
+
+        $stmt = $myConnexion->prepare("SELECT count(*) as number FROM user");
+        $state = $stmt->execute();
+
+        if ($state) {
+            $count = $stmt->fetch();
+            echo "Il y a : " . $count['number']. "utilisateurs". "<br>";
+        }
+
+        $stmt = $myConnexion->prepare("SELECT count(*) as number FROM user WHERE numero >= 5");
+        $state = $stmt->execute();
+
+        if ($state) {
+            $count = $stmt->fetch();
+            echo "Il y a : " . $count['number']. "utilisateurs ayant un numéro de rue plus grand ou égal à 5" . "<br>";
+        }
+
+        $stmt = $myConnexion->prepare("SELECT AVG(age) as moyenne_age FROM user");
+        $state = $stmt->execute();
+
+        if ($state) {
+            $average = $stmt->fetch();
+            echo "L'âge moyen est  " . $average['moyenne_age']. "ans". "<br>";
+        }
+
+        $stmt = $myConnexion->prepare("SELECT SUM(numero) as somme_numero FROM user");
+        $state = $stmt->execute();
+
+        if ($state) {
+            $sum = $stmt->fetch();
+            echo "La somme des numéros de maison est : " . $sum['somme_numero']. "<br>";
+        }
+    }
+    catch (PDOException $exception) {
+        echo $exception->getMessage();
+    }
+
 
 
     ?>
